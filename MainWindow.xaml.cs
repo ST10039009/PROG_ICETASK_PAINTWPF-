e7462a6414910b2625b68805095797843aa1976b
ST10039009 <104620510+ST10039009@users.noreply.github.com>
2023-06-02 17:25:24 +0200

    uploaded vis upload tool

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Problemoftheday22May
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EraseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
